0b2352180542e7ff92ee1672bf179ef870348bc0
Creeper-web <fayemiibukun@gmail.com>
2025-04-19 16:12:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Creeper', // Your name
   ownerNumber: '2348034723356', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
