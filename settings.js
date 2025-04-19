18252c2f509507822f8ca5b03090b4807bd41c6a
Creeper-web <fayemiibukun@gmail.com>
2025-04-19 17:19:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Creeper', // Your name
-  ownerNumber: '2348034723356', //Your number
+  ownerNumber: '+2348034723356', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
